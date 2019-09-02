557d1b9cd9a5debddde640cd28a03b32d662a43d
ithpip <afifceo@gmail.com>
2019-09-02 08:58:37 +0700

    head animation

--- app/src/main/java/com/petapurba/MainActivity.java
+++ app/src/main/java/com/petapurba/MainActivity.java
@@ -3,22 +3,23 @@ package com.petapurba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     Dialog dialog;
     Button menu;
-    ToggleButton tgl1;
-    FrameLayout detail1;
+    ToggleButton tgl1,tgl2,tgl3,tgl4;
+    FrameLayout detail1,detail2,detail3,detail4;
+    ImageView zoom1,zoom2,zoom3,zoom4;
 
 
     @Override
@@ -28,11 +29,26 @@ public class MainActivity extends AppCompatActivity {
 
         menu = findViewById(R.id.menu1);
         tgl1 = findViewById(R.id.toggleButton1);
+        tgl2 = findViewById(R.id.toggleButton2);
+        tgl3 = findViewById(R.id.toggleButton3);
+        tgl4 = findViewById(R.id.toggleButton4);
         detail1 = findViewById(R.id.detail1);
+        detail2 = findViewById(R.id.detail2);
+        detail3 = findViewById(R.id.detail3);
+        detail4 = findViewById(R.id.detail4);
+        zoom1 = findViewById(R.id.zoom1);
+        zoom2 = findViewById(R.id.zoom2);
+        zoom3 = findViewById(R.id.zoom3);
+        zoom4 = findViewById(R.id.zoom4);
         dialog = new Dialog(this);
 //        dialog = new Dialog(this, );
 //        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        final Animation zoomAnimation = AnimationUtils.loadAnimation(this,R.anim.zoom);
+        zoom1.startAnimation(zoomAnimation);
+        zoom2.startAnimation(zoomAnimation);
+        zoom3.startAnimation(zoomAnimation);
+        zoom4.startAnimation(zoomAnimation);
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,18 +57,128 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        tgl1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        tgl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (detail1.getVisibility() == View.INVISIBLE){
+                    detail1.setVisibility(View.VISIBLE);
+                    detail2.setVisibility(View.INVISIBLE);
+                    detail3.setVisibility(View.INVISIBLE);
+                    detail4.setVisibility(View.INVISIBLE);
+                }else {
+                    detail1.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        tgl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (detail2.getVisibility() == View.INVISIBLE){
+                    detail2.setVisibility(View.VISIBLE);
+                    detail1.setVisibility(View.INVISIBLE);
+                    detail3.setVisibility(View.INVISIBLE);
+                    detail4.setVisibility(View.INVISIBLE);
+                } else {
+                    // The toggle is disabled
+
+                    detail2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        tgl3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (detail3.getVisibility() == View.INVISIBLE){
+                    detail3.setVisibility(View.VISIBLE);
+                    detail1.setVisibility(View.INVISIBLE);
+                    detail2.setVisibility(View.INVISIBLE);
+                    detail4.setVisibility(View.INVISIBLE);
+                } else {
+                    // The toggle is disabled
+
+                    detail3.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        tgl4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (detail4.getVisibility() == View.INVISIBLE){
+                    detail4.setVisibility(View.VISIBLE);
+                    detail1.setVisibility(View.INVISIBLE);
+                    detail2.setVisibility(View.INVISIBLE);
+                    detail3.setVisibility(View.INVISIBLE);
+                } else {
+                    // The toggle is disabled
+
+                    detail4.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        /*tgl1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // The toggle is enabled
                     detail1.setVisibility(View.VISIBLE);
+                    detail2.setVisibility(View.INVISIBLE);
+                    detail3.setVisibility(View.INVISIBLE);
+                    detail4.setVisibility(View.INVISIBLE);
                 } else {
                     // The toggle is disabled
 
                     detail1.setVisibility(View.INVISIBLE);
                 }
             }
-        });
+        });*/
+        /*tgl2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    detail2.setVisibility(View.VISIBLE);
+                    detail1.setVisibility(View.INVISIBLE);
+                    detail3.setVisibility(View.INVISIBLE);
+                    detail4.setVisibility(View.INVISIBLE);
+                } else {
+                    // The toggle is disabled
+
+                    detail2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });*/
+        /*tgl3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    detail3.setVisibility(View.VISIBLE);
+                    detail1.setVisibility(View.INVISIBLE);
+                    detail2.setVisibility(View.INVISIBLE);
+                    detail4.setVisibility(View.INVISIBLE);
+                } else {
+                    // The toggle is disabled
+
+                    detail3.setVisibility(View.INVISIBLE);
+                }
+            }
+        });*/
+        /*tgl4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    detail4.setVisibility(View.VISIBLE);
+                    detail1.setVisibility(View.INVISIBLE);
+                    detail2.setVisibility(View.INVISIBLE);
+                    detail3.setVisibility(View.INVISIBLE);
+                } else {
+                    // The toggle is disabled
+
+                    detail4.setVisibility(View.INVISIBLE);
+                }
+            }
+        });*/
 
 
     }
---
+++ app/src/main/java/com/petapurba/PengetahuanAwal.java
@@ -0,0 +1,14 @@
+package com.petapurba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PengetahuanAwal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengetahuan_awal);
+    }
+}
